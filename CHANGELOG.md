e14bfdf875edbc8b351385690d70ffac0ef2f941
Arkadiusz Kondas <arkadiusz.kondas@gmail.com>
2020-04-02 07:45:23 +0200

    Fix scope separator

---
+++ CHANGELOG.md
@@ -0,0 +1,19 @@
+# Changelog
+
+All notable changes to this project will be documented in this file.
+
+The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
+and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
+
+## [0.1.1] - 2020-04-02
+### Added
+- all scopes as const
+- buddy.yml
+
+### Fixed
+- scope separator
+- fetch resource owner (missing token) - fixed with BearerAuthorizationTrait 
+
+## [0.1.0] - 2020-04-01
+### Added
+- First initial release 
--- src/Provider/Buddy.php
+++ src/Provider/Buddy.php
@@ -7,11 +7,34 @@ namespace Buddy\OAuth2\Client\Provider;
 use Buddy\OAuth2\Client\Provider\Exception\BuddyIdentityProviderException;
 use League\OAuth2\Client\Provider\AbstractProvider;
 use League\OAuth2\Client\Token\AccessToken;
+use League\OAuth2\Client\Tool\BearerAuthorizationTrait;
 use Psr\Http\Message\ResponseInterface;
 
 final class Buddy extends AbstractProvider
 {
-    public const SCOPE_USER_INFO = 'USER_INFO';
+    use BearerAuthorizationTrait;
+
+    public const SCOPE_WORKSPACE = 'SCOPE_WORKSPACE';
+    public const SCOPE_PROJECT_DELETE = 'SCOPE_PROJECT_DELETE';
+    public const SCOPE_REPOSITORY_READ = 'SCOPE_REPOSITORY_READ';
+    public const SCOPE_REPOSITORY_WRITE = 'SCOPE_REPOSITORY_WRITE';
+    public const SCOPE_EXECUTION_INFO = 'SCOPE_EXECUTION_INFO';
+    public const SCOPE_EXECUTION_RUN = 'SCOPE_EXECUTION_RUN';
+    public const SCOPE_EXECUTION_MANAGE = 'SCOPE_EXECUTION_MANAGE';
+    public const SCOPE_USER_INFO = 'SCOPE_USER_INFO';
+    public const SCOPE_USER_KEY = 'SCOPE_USER_KEY';
+    public const SCOPE_USER_EMAIL = 'SCOPE_USER_EMAIL';
+    public const SCOPE_INTEGRATION_INFO = 'SCOPE_INTEGRATION_INFO';
+    public const SCOPE_MEMBER_EMAIL = 'SCOPE_MEMBER_EMAIL';
+    public const SCOPE_MANAGE_EMAILS = 'SCOPE_MANAGE_EMAILS';
+    public const SCOPE_WEBHOOK_INFO = 'SCOPE_WEBHOOK_INFO';
+    public const SCOPE_WEBHOOK_ADD = 'SCOPE_WEBHOOK_ADD';
+    public const SCOPE_WEBHOOK_MANAGE = 'SCOPE_WEBHOOK_MANAGE';
+    public const SCOPE_VARIABLE_ADD = 'SCOPE_VARIABLE_ADD';
+    public const SCOPE_VARIABLE_INFO = 'SCOPE_VARIABLE_INFO';
+    public const SCOPE_VARIABLE_MANAGE = 'SCOPE_VARIABLE_MANAGE';
+
+    private const SCOPE_SEPARATOR = ' '; // will be encoded as + (Buddy use + as scope separator)
 
     /**
      * @var string
@@ -72,5 +95,10 @@ final class Buddy extends AbstractProvider
     protected function createResourceOwner(array $response, AccessToken $token)
     {
         return new BuddyResourceOwner($response);
+    }
+
+    protected function getScopeSeparator()
+    {
+        return self::SCOPE_SEPARATOR;
     }
 }
